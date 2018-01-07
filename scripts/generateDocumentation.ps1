342b616eda2a8e9e127553023770546413c4f327
djfdyuruiry <djfdyuruiry@hotmail.com>
2018-01-07 16:36:52 +0000

    Revert "Adding tasks and simplifying WSL calls"
    This reverts commit 044bedaa410f78bc104d859f84d9ba72420a155b, reversing
    changes made to ae5075d58754b9089968ad30f442e0cb2d15310f.

--- scripts/generateDocumentation.ps1
+++ scripts/generateDocumentation.ps1
@@ -1,2 +1,1 @@
-#luadoc "$PSScriptRoot/../src/*.lua" "$PSScriptRoot/../src/*/*/*.lua" "$PSScriptRoot/../src/*/*.lua" -d "$PSScriptRoot/../build/release/luadoc"
-# TODO: fix luadoc for Lua5.2/3 or remove
+luadoc "$PSScriptRoot/../src/*.lua" "$PSScriptRoot/../src/*/*/*.lua" "$PSScriptRoot/../src/*/*.lua" -d "$PSScriptRoot/../build/release/luadoc"
